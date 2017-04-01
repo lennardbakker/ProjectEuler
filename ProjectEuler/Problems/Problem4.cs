2bc8932c8ff1ab9d35a22d89da442b131b45116a
L. Bakker <lennardbakker1997@gmail.com>
2017-04-01 23:48:28 +0200

    +Check if palindrome methode, additions within our double-loop

--- ProjectEuler/Problems/Problem4.cs
+++ ProjectEuler/Problems/Problem4.cs
@@ -13,17 +13,35 @@ namespace ProjectEuler.Problems
             var _firstProduct = 100;
             var _secondProduct = 100;
             var _threeDigitRange = 999;
+            var _highestFound = 0;
 
             for (int i = _threeDigitRange; i >= _firstProduct; i--)
             {
                 for (int j = _threeDigitRange; j >= _secondProduct; j--)
                 {
-                    //If i * j = Palindrome number
-                    //Then print sum and break;
+                    int _sum = i * j;
+                    string _sumString = _sum.ToString();
+
+                    if (isPalindrome(_sumString) && (_sum > _highestFound))
+                    {
+                        _highestFound = _sum;
+                    }
+
                 }
             }
-           
+            Console.WriteLine(_highestFound);
             Console.ReadKey();
+        }
+
+        public bool isPalindrome(string myString)
+        {
+            string _first = myString.Substring(0, myString.Length / 2);
+            char[] _arr = myString.ToCharArray();
+            Array.Reverse(_arr);
+            string _temp = new string(_arr);
+            string _second = _temp.Substring(0, _temp.Length / 2);
+
+            return _first.Equals(_second);
         }
     }
 }
