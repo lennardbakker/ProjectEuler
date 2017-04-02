586a166781b5ba4c830c4ba9c4f92874b1c5fa34
L. Bakker <lennardbakker1997@gmail.com>
2017-04-02 21:50:38 +0200

    +Problem3 init

---
+++ ProjectEuler/Problems/Problem3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    public class Problem3
+    {
+        public Problem3()
+        {
+            //init
+        }
+    }
+}
