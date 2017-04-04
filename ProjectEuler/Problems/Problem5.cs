fa3851e62860a0074eaca0b2caea68c2b48501f0
L. Bakker <lennardbakker1997@gmail.com>
2017-04-04 23:32:14 +0200

    +Problem 5

---
+++ ProjectEuler/Problems/Problem5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    public class Problem5
+    {
+        public Problem5()
+        {
+
+        }
+    }
+}
